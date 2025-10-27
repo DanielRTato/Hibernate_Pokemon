fe0e9e04e61c208a1ad2d4d68b5b836ad21ea94c
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-10-27 14:32:15 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,18 @@ public class Main {
 
         PokedexServices pokedexS = new PokedexServices();
 
-        Pokedex p1 = new Pokedex("Charmander", new BigDecimal(49) , "df");
-        Pokedex p2 = new Pokedex("Bulbasur", new BigDecimal(4119) , "sdfds");
-        Pokedex p3 = new Pokedex("Pikachu", new BigDecimal(419) , "sdfsdfsdf");
-        Pokedex p4 = new Pokedex("Charmander", new BigDecimal(149) , "sdf");
-        Pokedex p5 = new Pokedex("Charizard", new BigDecimal(3232) , "hj");
-        Pokedex p6 = new Pokedex("Mew", new BigDecimal(31) , "i");
-        Pokedex p7 = new Pokedex("Evee", new BigDecimal(123123) , "nmb");
-        Pokedex p8 = new Pokedex("MewTwo", new BigDecimal(332) , "asd");
-        Pokedex p9 = new Pokedex("Vulkarona", new BigDecimal(222) , "fgdg");
-        Pokedex p10 = new Pokedex("persian", new BigDecimal(1) , "fgh");
+        pokedexS.crearPokedex("Charmander", new BigDecimal(49) , "df");
+        pokedexS.crearPokedex("Bulbasur", new BigDecimal(4119) , "sdfds");
+        pokedexS.crearPokedex("Pikachu", new BigDecimal(419) , "sdfsdfsdf");
+        pokedexS.crearPokedex("Charmander", new BigDecimal(149) , "sdf");
+        pokedexS.crearPokedex("Charizard", new BigDecimal(3232) , "hj");
+        pokedexS.crearPokedex("Mew", new BigDecimal(31) , "i");
+        pokedexS.crearPokedex("Evee", new BigDecimal(123123) , "nmb");
+        pokedexS.crearPokedex("MewTwo", new BigDecimal(332) , "asd");
+        pokedexS.crearPokedex("Vulkarona", new BigDecimal(222) , "fgdg");
+        pokedexS.crearPokedex("persian", new BigDecimal(1) , "fgh");
 
-        pokedexS.listarPokedexConsulta();
+
 
 
     }
