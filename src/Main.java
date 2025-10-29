c5385d463ff94edc8c9aab9391f9ef8cceb616a8
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-10-29 14:07:41 +0100

    Parte1 fin

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,35 @@ public class Main {
 
         PokedexServices pokedexS = new PokedexServices();
 
-        pokedexS.crearPokedex("Charmander", new BigDecimal(49) , "df");
-        pokedexS.crearPokedex("Bulbasur", new BigDecimal(4119) , "sdfds");
-        pokedexS.crearPokedex("Pikachu", new BigDecimal(419) , "sdfsdfsdf");
-        pokedexS.crearPokedex("Charmander", new BigDecimal(149) , "sdf");
-        pokedexS.crearPokedex("Charizard", new BigDecimal(3232) , "hj");
-        pokedexS.crearPokedex("Mew", new BigDecimal(31) , "i");
-        pokedexS.crearPokedex("Evee", new BigDecimal(123123) , "nmb");
-        pokedexS.crearPokedex("MewTwo", new BigDecimal(332) , "asd");
-        pokedexS.crearPokedex("Vulkarona", new BigDecimal(222) , "fgdg");
-        pokedexS.crearPokedex("persian", new BigDecimal(1) , "fgh");
+//        pokedexS.crearPokedex("Charmander", new BigDecimal(49) , "df");
+//        pokedexS.crearPokedex("Bulbasur", new BigDecimal(4119) , "sdfds");
+//        pokedexS.crearPokedex("Pikachu", new BigDecimal(419) , "sdfsdfsdf");
+//        pokedexS.crearPokedex("Charmander", new BigDecimal(149) , "sdf");
+//        pokedexS.crearPokedex("Charizard", new BigDecimal(3232) , "hj");
+//        pokedexS.crearPokedex("Mew", new BigDecimal(31) , "i");
+//        pokedexS.crearPokedex("Evee", new BigDecimal(123123) , "nmb");
+//        pokedexS.crearPokedex("MewTwo", new BigDecimal(332) , "asd");
+//        pokedexS.crearPokedex("Vulkarona", new BigDecimal(222) , "fgdg");
+//        pokedexS.crearPokedex("persian", new BigDecimal(1) , "fgh");
 
+        for (Pokedex p : pokedexS.listarPokedexConsulta()) {
+            System.out.println(p);
+        }
+
+       Pokedex charmander = pokedexS.leerPokedex(1l);
+        charmander.setMisc("Actualizado");
+
+        Pokedex pikachu = pokedexS.leerPokedex(3l);
+        pikachu.setMisc("Segundo actualizado");
+
+        for (Pokedex p : pokedexS.listarPokedexConsulta()) {
+            System.out.println(p);
+        }
+
+        pokedexS.actualizarPokedex(charmander);
+        pokedexS.actualizarPokedex(pikachu);
+
+        pokedexS.eliminarConsulta();
 
 
 
