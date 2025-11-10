f409b4efa27c98a79b9e7b49cbfc0c82ed7e1236
DanielRTato <drodrigueztato@danielcastelao.org>
2025-11-10 19:51:28 +0100

    Refactor SerializarPokedex to support serialization and deserialization of multiple Pokedex objects

--- src/util/SerializarPokedex.java
+++ src/util/SerializarPokedex.java
@@ -3,17 +3,36 @@ package util;
 import model.Pokedex;
 import model.Pokemon;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SerializarPokedex {
 
-    public static void serializarPokedex(String ruta) {
+    public static void serializarPokedex(List<Pokedex> lista, String ruta) {
 
-        List<Pokedex> listaPokemon = new ArrayList<>();
-        String consulta = "select * from pokemon";
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta))){
+            oos.writeObject(lista.get(0));
+            oos.writeObject(lista.get(1));
+            System.out.println("Serialización de 2 Pokedex terminada:  " + ruta );
+        } catch (Exception e){
+            System.out.println("Error durante la serialización: " + e.getMessage());
+        }
+    }
 
-
+    public static Pokedex[] deserializarPokedex(String ruta) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta))) {
+            Pokedex p1 = (Pokedex) ois.readObject();
+            Pokedex p2 = (Pokedex) ois.readObject();
+            System.out.println("Deserializadas 2 Pokedex: " + ruta);
+            return new Pokedex[]{p1, p2};
+        } catch (Exception e) {
+            System.out.println("Error deserializando: " + e.getMessage());
+            return new Pokedex[0];
+        }
     }
 }
